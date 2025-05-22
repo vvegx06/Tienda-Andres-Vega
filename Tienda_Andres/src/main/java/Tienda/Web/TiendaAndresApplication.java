3ec39a88e2c9c70c1cc0d668e6c27e7fc93088bb
Andres Vega <andresvegamunoz2206@gmail.com>
2025-05-21 19:14:40 -0600

    Commit 1
    Mi primer commit

---
+++ Tienda_Andres/src/main/java/Tienda/Web/TiendaAndresApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAndresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAndresApplication.class, args);
+	}
+
+}
