3b873424290c639c4f8d1ce85655569719177e02
Andres Vega <andresvegamunoz2206@gmail.com>
2025-07-02 20:44:00 -0600

    commit sem 8

---
+++ Tienda_Andres/src/main/java/Tienda/Web/dao/ProductoDao.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda.Web.dao;
+
+import Tienda.Web.domain.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoDao extends JpaRepository<Producto, Long> {
+
+}
---
+++ Tienda_Andres/src/main/java/Tienda/Web/service/impl/ProductoServiceImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda.Web.service.impl;
+
+import Tienda.Web.dao.ProductoDao;
+import Tienda.Web.domain.Producto;
+import Tienda.Web.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+     @Autowired
+    private ProductoDao productoDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activos) {
+        var lista = productoDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Producto getProducto(Producto producto) {
+        return productoDao.findById(producto.getIdProducto()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Producto producto) {
+        productoDao.save(producto);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Producto producto) {
+        productoDao.delete(producto);
+    }
+    
+}
+ 
